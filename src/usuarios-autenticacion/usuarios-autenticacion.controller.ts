c90a475bd6e03f9f5edec2d1cb5f1b252071d068
Zulu <juanpablozuluaga.0311@gmail.com>
2025-10-27 22:01:11 -0500

    Feat: Se ha añadido y probado endpoint de crearUsuario y DTO.

--- src/usuarios-autenticacion/usuarios-autenticacion.controller.ts
+++ src/usuarios-autenticacion/usuarios-autenticacion.controller.ts
@@ -9,20 +9,30 @@ import { catchError } from 'rxjs';
 export class UsuariosAutenticacionController {
   constructor(
     @Inject(NATS_SERVICE) private readonly client: ClientProxy
-) {}
+  ) { }
 
   //NOTA: SE DEBE HACER UN DTO para cada cosa que se reciba dentro del MS
   //Se hace en el MS y luego se copia y se pega exactamente eso mismo aca,
-  @Post('crearUsuario')
+  /*@Post('crearUsuario')
   create(@Body() createUsuariosAutenticacionDto: CreateUsuariosAutenticacionDto) {
     //El primer parametro siempre es lo que se pone en el MessagePattern(EventPattern)
     //El segundo parametro es el payload
     return this.client.send({cmd:'createUsuariosAutenticacion'},{})
+  } */
+
+  @Post('crearUsuario')
+  create(@Body() dto: CreateUsuariosAutenticacionDto) {
+    return this.client
+      .send({ cmd: 'createUsuariosAutenticacion' }, dto)
+      .pipe(
+        catchError(err => { throw new RpcException(err); })
+      );
   }
+
 
   @Get('buscarUsuarios')
   findAll() {
-    return this.client.send({cmd:'findAllUsuariosAutenticacion'},{})
+    return this.client.send({ cmd: 'findAllUsuariosAutenticacion' }, {})
   }
 
   @Get('buscarUsuario/:id')
@@ -30,22 +40,22 @@ export class UsuariosAutenticacionController {
     //Tener mucho cuidado con el tipo de dato del Id, importante parsear
     //para que cuando se hagan consultas a BD no de error
     //IMPORTANTE: hacer el throw del RpcException y asegurarse de estar haciendo el throw en el microservicio para que lance la excepción que es, REVISAR EL RESTO DEL CÓDIGO (descripciones y el otro repo) PARA ENTENDER
-    return this.client.send({cmd:'findOneUsuariosAutenticacion'},{id}) 
-    .pipe(
-      catchError(err => {
-        throw new RpcException(err);
-      }
+    return this.client.send({ cmd: 'findOneUsuariosAutenticacion' }, { id })
+      .pipe(
+        catchError(err => {
+          throw new RpcException(err);
+        }
+        )
       )
-    )
   }
 
   @Patch('actualizarPerfil/:id')
   update(@Param('id') id: string, @Body() updateUsuariosAutenticacionDto: UpdateUsuariosAutenticacionDto) {
-    return this.client.send({cmd:'updateUsuariosAutenticacion'},{})
+    return this.client.send({ cmd: 'updateUsuariosAutenticacion' }, {})
   }
 
   @Delete('borrarPerfil/:id')
   remove(@Param('id') id: string) {
-    return this.client.send({cmd:'removeUsuariosAutenticacion'},{})
+    return this.client.send({ cmd: 'removeUsuariosAutenticacion' }, {})
   }
 }
