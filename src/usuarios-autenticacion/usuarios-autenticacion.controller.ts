bfeb70522c60aa8c807eb527c73e51815b6a1734
Zulu <juanpablozuluaga.0311@gmail.com>
2025-10-29 10:00:55 -0500

    Feat: Se añadió la lógica de inicio de sesión

--- src/usuarios-autenticacion/usuarios-autenticacion.controller.ts
+++ src/usuarios-autenticacion/usuarios-autenticacion.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CreateUsuariosAutenticacionDto } from './dto/create-usuarios-autenticacion.dto';
 import { UpdateUsuariosAutenticacionDto } from './dto/update-usuarios-autenticacion.dto';
 import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
+import { loginUsuarioDto } from './dto/login-usuario.dto';
 
 @Controller('usuarios-autenticacion')
 export class UsuariosAutenticacionController {
-  constructor(
-    @Inject(NATS_SERVICE) private readonly client: ClientProxy
-  ) { }
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   //NOTA: SE DEBE HACER UN DTO para cada cosa que se reciba dentro del MS
   //Se hace en el MS y luego se copia y se pega exactamente eso mismo aca,
@@ -22,17 +31,24 @@ export class UsuariosAutenticacionController {
 
   @Post('crearUsuario')
   create(@Body() dto: CreateUsuariosAutenticacionDto) {
-    return this.client
-      .send({ cmd: 'createUsuariosAutenticacion' }, dto)
-      .pipe(
-        catchError(err => { throw new RpcException(err); })
-      );
+    return this.client.send({ cmd: 'createUsuariosAutenticacion' }, dto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
-
+  @Post('login')
+  login(@Body() dto: loginUsuarioDto) {
+    return this.client.send({ cmd: 'loginUsuario' }, dto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
 
   @Get('buscarUsuarios')
   findAll() {
-    return this.client.send({ cmd: 'findAllUsuariosAutenticacion' }, {})
+    return this.client.send({ cmd: 'findAllUsuariosAutenticacion' }, {});
   }
 
   @Get('buscarUsuario/:id')
@@ -40,22 +56,25 @@ export class UsuariosAutenticacionController {
     //Tener mucho cuidado con el tipo de dato del Id, importante parsear
     //para que cuando se hagan consultas a BD no de error
     //IMPORTANTE: hacer el throw del RpcException y asegurarse de estar haciendo el throw en el microservicio para que lance la excepción que es, REVISAR EL RESTO DEL CÓDIGO (descripciones y el otro repo) PARA ENTENDER
-    return this.client.send({ cmd: 'findOneUsuariosAutenticacion' }, { id })
+    return this.client
+      .send({ cmd: 'findOneUsuariosAutenticacion' }, { id })
       .pipe(
-        catchError(err => {
+        catchError((err) => {
           throw new RpcException(err);
-        }
-        )
-      )
+        }),
+      );
   }
 
   @Patch('actualizarPerfil/:id')
-  update(@Param('id') id: string, @Body() updateUsuariosAutenticacionDto: UpdateUsuariosAutenticacionDto) {
-    return this.client.send({ cmd: 'updateUsuariosAutenticacion' }, {})
+  update(
+    @Param('id') id: string,
+    @Body() updateUsuariosAutenticacionDto: UpdateUsuariosAutenticacionDto,
+  ) {
+    return this.client.send({ cmd: 'updateUsuariosAutenticacion' }, {});
   }
 
   @Delete('borrarPerfil/:id')
   remove(@Param('id') id: string) {
-    return this.client.send({ cmd: 'removeUsuariosAutenticacion' }, {})
+    return this.client.send({ cmd: 'removeUsuariosAutenticacion' }, {});
   }
 }
