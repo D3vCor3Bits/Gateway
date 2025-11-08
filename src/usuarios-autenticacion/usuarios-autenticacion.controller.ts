1701d30db697b1e2d5b61423a6df630de388f09f
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2025-11-08 03:18:59 -0500

    refactor: Simplify authorization handling and remove unused methods in UsuariosAutenticacionController

--- src/usuarios-autenticacion/usuarios-autenticacion.controller.ts
+++ src/usuarios-autenticacion/usuarios-autenticacion.controller.ts
@@ -25,7 +25,7 @@ import { crearInvitacionDto } from './dto/crear-invitacion.dto';
 
 @Controller('usuarios-autenticacion')
 export class UsuariosAutenticacionController {
-  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) { }
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('crearUsuario')
   create(@Body() dto: CreateUsuariosAutenticacionDto) {
@@ -45,29 +45,20 @@ export class UsuariosAutenticacionController {
   }
 
   @Get('buscarUsuarios')
-  findAll(@Req() req: Request) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      throw new RpcException('Token no proporcionado');
-    }
-    const token = authHeader.replace('Bearer ', '');
-
-    return this.client
-      .send({ cmd: 'findUsers' }, { token })
-      .pipe(catchError((err) => { throw new RpcException(err); }));
+  findAll() {
+    return this.client.send({ cmd: 'findAllUsuariosAutenticacion' }, {});
   }
 
   @Get('buscarUsuario/:id')
-  findOne(@Req() req: Request, @Param('id', ParseUUIDPipe) id: string) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      throw new RpcException('Token no proporcionado');
-    }
-    const token = authHeader.replace('Bearer ', '');
-
-    return this.client
-      .send({ cmd: 'findUserById' }, { token, id })
-      .pipe(catchError((err) => { throw new RpcException(err); }));
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    //Tener mucho cuidado con el tipo de dato del Id, importante parsear
+    //para que cuando se hagan consultas a BD no de error
+    //IMPORTANTE: hacer el throw del RpcException y asegurarse de estar haciendo el throw en el microservicio para que lance la excepción que es, REVISAR EL RESTO DEL CÓDIGO (descripciones y el otro repo) PARA ENTENDER
+    return this.client.send({ cmd: 'findUserById' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Patch('actualizarPerfil/:id')
@@ -104,14 +95,14 @@ export class UsuariosAutenticacionController {
   }
 
   @Get('medicoPaciente/:idPaciente')
-  traerMedicoDePaciente(@Req() req: Request, @Param('idPaciente', ParseUUIDPipe) idPaciente: string) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) throw new RpcException('Token no proporcionado');
-    const token = authHeader.replace('Bearer ', '');
-
-    return this.client
-      .send({ cmd: 'pacienteMedico' }, { token, idPaciente })
-      .pipe(catchError((err) => { throw new RpcException(err); }));
+  traerMedicoDeCuidador(
+    @Param('idPaciente', ParseUUIDPipe) idPaciente: string,
+  ) {
+    return this.client.send({ cmd: 'pacienteMedico' }, { idPaciente }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
   @Post('asignarCuidador')
   asignarCuidador(@Body() dto: asignarCuidadorPacienteDto) {
@@ -140,14 +131,8 @@ export class UsuariosAutenticacionController {
   }
 
   @Get('pacientesDeMedico/:idMedico')
-  pacientesMedico(@Req() req: Request, @Param('idMedico', ParseUUIDPipe) idMedico: string) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) throw new RpcException('Token no proporcionado');
-    const token = authHeader.replace('Bearer ', '');
-
-    return this.client
-      .send({ cmd: 'pacientesMedico' }, { token, idMedico })
-      .pipe(catchError((err) => { throw new RpcException(err); }));
+  pacientesMedico(@Param('idMedico', ParseUUIDPipe) idMedico: string) {
+    return this.client.send({ cmd: 'pacientesMedico' }, { idMedico });
   }
 
   @Get('totalUsuarios')
@@ -164,8 +149,8 @@ export class UsuariosAutenticacionController {
     );
   }
 
-
-  @Get('perfil')
+  
+  @Post('perfil')
   async obtenerPerfil(@Req() req: Request) {
     const authHeader = req.headers.authorization;
 
@@ -181,34 +166,5 @@ export class UsuariosAutenticacionController {
       }),
     );
   }
-  @Post('subirFotoPerfil')
-  async subirImagenPerfil(@Req() req: Request, @Body('imagenUrl') imagenUrl: string) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      throw new RpcException('Token no proporcionado');
-    }
-
-    const token = authHeader.replace('Bearer ', '');
-
-    return this.client
-      .send({ cmd: 'uploadProfileImage' }, { token, imagenUrl })
-      .pipe(
-        catchError((err) => {
-          throw new RpcException(err);
-        }),
-      );
-  }
-  @Patch('desactivarUsuario')
-  async eliminarCuenta(@Req() req: Request) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      throw new RpcException('Token no proporcionado');
-    }
-    const token = authHeader.replace('Bearer ', '');
-    return this.client.send({ cmd: 'desactivarUsuario' }, { token }).pipe(
-      catchError((err) => {
-        throw new RpcException(err);
-      }),
-    );
-  }
+  
 }
